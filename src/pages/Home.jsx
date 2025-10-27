002762c975c01c25a5957d3735893359b93422e1
Jorge6725 <jorgehjacome13@gmail.com>
2025-10-27 19:45:47 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,19 +15,6 @@ export default function Home() {
           <h3>Conjunto Fitness Roxo</h3>
           <p>R$ 89,90</p>
         </div>
-
-        <div className="produto">
-          <img src="/imagens/blusa1.jpg" alt="Blusa Casual" />
-          <h3>Blusa Casual</h3>
-          <p>R$ 59,90</p>
-        </div>
-
-        <div className="produto">
-          <img src="/imagens/saia1.jpg" alt="Saia Midi" />
-          <h3>Saia Midi</h3>
-          <p>R$ 74,90</p>
-        </div>
-      </div>
     </div>
   );
 }
