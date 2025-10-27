86456d64f2774f0023cd95fc8a4fbb5e89b0274a
Jorge6725 <jorgehjacome13@gmail.com>
2025-10-27 19:19:45 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,30 @@
-// Conteúdo simulado para pages/Home.jsx
+import React from "react";
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div className="catalogo">
+      <h1>Nova Coleção Primavera 🌸</h1>
+
+      <div className="produtos">
+        <div className="produto">
+          <img src=" " alt="Vestido Floral" />
+          <h3>Vestido Floral</h3>
+          <p>R$ 89,90</p>
+        </div>
+
+        <div className="produto">
+          <img src=" " alt="Blusa Casual" />
+          <h3>Blusa Casual</h3>
+          <p>R$ 59,90</p>
+        </div>
+
+        <div className="produto">
+          <img src="" alt="Saia Midi" />
+          <h3>Saia Midi</h3>
+          <p>R$ 74,90</p>
+        </div>
+      </div>
+    </div>
+  );
+}
